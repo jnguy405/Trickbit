e53fae667575a83a9a8eec386535cd7abe6f66f6
jnguy405 <jnguy405@ucsc.edu>
2025-05-22 21:25:41 -0700

    (PHYSICS/LEVEL) Updated layer collision and animation

--- src/main.js
+++ src/main.js
@@ -1,22 +1,10 @@
-// Jim Whitehead
-// Created: 4/14/2024
-// Phaser: 3.70.0
-//
-// Cubey
-//
-// An example of putting sprites on the screen using Phaser
-// 
-// Art assets from Kenny Assets "Shape Characters" set:
-// https://kenney.nl/assets/shape-characters
-
-// debug with extreme prejudice
 "use strict";
 
 let config = {
     type: Phaser.AUTO,
     parent: 'phaser-game',
     width: 5120,
-    height: 1024,
+    height: 1280,
     physics: {
         default: 'arcade',
         arcade: {
